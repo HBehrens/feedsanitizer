599bdbc8a58128476639ef46b3e804e7a7d8e683
Heiko Behrens <HeikoBehrens@gmx.de>
2011-03-01 16:37:52 +0100

    better error handling (fixes #3)

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 
+import sys, traceback
 import logging
 import StringIO
 import feedgenerators
@@ -14,26 +15,28 @@ import feedparser
 from django.utils import feedgenerator
 
 def buildfeed(url, feed_class, feed_link):
-    result = feedparser.parse(url)      
+    feed = feedparser.parse(url)      
 
-    feed = feed_class(\
-        title=result.feed.title or "feed title",\
-        link=result.feed.link or "feed link",\
+    result = feed_class(\
+        title=result.feed.title if "title" in result.feed else "feed title",\
+        link=result.feed.link if "link" in result.feed else "feed link",\
         feed_url=feed_link,\
         description="", \
         )
-    for entry in result.entries:
+        
+    for entry in feed.entries:
         updated = datetime.fromtimestamp(mktime(entry.updated_parsed))
-        feed.add_item( \
+        item = result.add_item( \
           title = entry.title, \
           link = entry.link, \
           description = entry.summary, \
-          author_name = entry.author, \
           pubdate = updated, \
           unique_id = entry.id\
           )
+        if "author" in entry: 
+            item.author_name = entry.author;
           
-    return feed
+    return result
     
 feed_formats = {
     "rss": feedgenerators.Rss201rev2FeedModified,
@@ -52,20 +55,30 @@ def parse_request(request):
     feed_link = feed_link = "http://%s%s?%s" % ( request.get_host(), reverse("views.sanitize"), request.GET.urlencode() )
     return (url, format, feed_class, feed_link)
 
+def handle_error(request, error):
+    return render_to_response("home.html", {
+        "error":error
+        });
+
 def userfriendly(request):
     feed = None
     prettyxml = None
+    error = None
     (url, format, feed_class, feed_link) = parse_request(request)
     
     if url:
-        feed = buildfeed(url, feed_class, feed_link)
-        feedxml = StringIO.StringIO()
-        feed.write(feedxml, 'utf-8')
-        prettyxml = xml.dom.minidom.parseString(feedxml.getvalue()).toprettyxml()
+        try:
+            feed = buildfeed(url, feed_class, feed_link)
+            feedxml = StringIO.StringIO()
+            feed.write(feedxml, 'utf-8')
+            prettyxml = xml.dom.minidom.parseString(feedxml.getvalue()).toprettyxml()
+        except Exception:
+            e = sys.exc_info()
+            error = traceback.format_exc()
         
     return render_to_response("home.html", {
         "feed": feed, "feed_xml": prettyxml, "feed_link": feed_link, "feed_format": format, 
-        "first_url": url}
+        "first_url": url, "error": error}
         )
 
 def sanitize(request):
